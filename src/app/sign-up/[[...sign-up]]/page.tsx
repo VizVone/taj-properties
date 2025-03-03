3b409aaf880dd7f73033659722b8d7b270c9ecfd
Omkar More <omkarmore7391@gmail.com>
2025-03-03 18:14:33 +0530

    second commit

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center h-screen bg-primary">
+    <div className="flex justify-center items-center h-screen bg-black">
       <SignUp />
     </div>
   );
