e2bc687c58012294f2c057a43611a61fcc634231
Omkar More <omkarmore7391@gmail.com>
2025-03-06 00:30:40 +0530

    updated-mobileview

--- src/app/(private)/user/queries/_components/user-queries-table.tsx
+++ src/app/(private)/user/queries/_components/user-queries-table.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { Property, Query } from "@prisma/client";
+import { Query, Property } from "@prisma/client";
 import { Table } from "antd";
 import dayjs from "dayjs";
 import React from "react";
 
-function UserQueriesTable({ queries }: { queries: Query[] }) {
+function UserQueriesTable({ queries }: { queries: (Query & { property: Property })[] }) {
   const columns = [
     {
       title: "Property",
@@ -27,9 +27,25 @@ function UserQueriesTable({ queries }: { queries: Query[] }) {
         dayjs(createdAt).format("DD MMM YYYY hh:mm A"),
     },
   ];
+
   return (
     <div>
-      <Table columns={columns} dataSource={queries} />
+      {/* Large screen table */}
+      <div className="hidden md:block">
+        <Table columns={columns} dataSource={queries} />
+      </div>
+      
+      {/* Small screen cards */}
+      <div className="md:hidden flex flex-col gap-4">
+        {queries.map((query) => (
+          <div key={query.id} className="bg-white shadow-md p-4 rounded-lg">
+            <p className="text-gray-800 font-semibold">Property: {query.property.name}</p>
+            <p className="text-gray-600">Quote Amount: $ {query.quoteAmount}</p>
+            <p className="text-gray-600">Message: {query.message}</p>
+            <p className="text-gray-600">Date: {dayjs(query.createdAt).format("DD MMM YYYY hh:mm A")}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
